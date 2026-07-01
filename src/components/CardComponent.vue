d8bfee0dcb5c2c5e8dd683efca7328f8a37d30a7
Paulo Grabin <plgrabin@gmail.com>
2026-06-30 21:13:30 -0300

    Add card dashboard to top of the page

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dashboard-card">
+    <div class="label">Coffee value</div>
+    <div class="label">Freight</div>
+    <div class="label">Markup</div>
+    <div class="label">Participants</div>
+    <div class="label">Total</div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,37 @@
 import ParticipantTable from '@/components/ParticipantTable.vue'
 import CoffeeTable from '@/components/CoffeeTable.vue'
 import CostsTable from '@/components/CostsTable.vue'
+import CardComponent from '@/components/CardComponent.vue'
 </script>
 
 <template>
-<!--  <main>-->
-    <ParticipantTable />
-    <CoffeeTable />
-    <CostsTable />
-<!--  </main>-->
+  <div class="dashboard">
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+    <CardComponent/>
+  </div>
+
+  <div class="two-columns">
+    <div class="column">
+      <ParticipantTable />
+      <CostsTable />
+    </div>
+    <div class="column">
+      <CoffeeTable />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.two-columns {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+}
+
+.column {
+  min-width: 0;
+  overflow: hidden;
+}
+</style>
